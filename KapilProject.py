44d4acb61c14e8dac1a9a9303ddd3e6f41194e8d
Kapil Arya <kapilarya@Kapils-MacBook-Air.local>
2021-12-16 16:21:13 +0530

    Initial commit Testing

---
+++ KapilProject.py
@@ -0,0 +1,25 @@
+a=2
+b=2
+
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+
+print(add,sub,mul,div)
+
+print("Hello World")
+
+import pandas as pd
+
+#Employee dataset imported from Kaggle - https://www.kaggle.com/tejashvi14/employee-future-prediction
+
+data=pd.read_csv("Employee.csv.xls")
+
+print(data.shape)
+
+k = data.head()
+
+print(k)
+
+
